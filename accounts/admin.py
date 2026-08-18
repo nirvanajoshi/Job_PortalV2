90426788bc07fe53b0cfd7f231409dc83839be90
nirvanajoshi <damodarj36@gmail.com>
2026-08-18 20:01:48 +0545

    Admin registration of profile

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'role', 'created_at']
+    list_filter = ['role', 'created_at']
+    search_fields = ['user__username', 'user__email']
+    readonly_fields = ['created_at', 'updated_at']
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'role', 'phone', 'profile_picture')
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
+    ordering = ['-created_at']
+    date_hierarchy = 'created_at'
