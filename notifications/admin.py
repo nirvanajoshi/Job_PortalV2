5af228b486ef4298b14b7ccb0443af477860fe88
nirvanajoshi <damodarj36@gmail.com>
2026-08-18 20:14:17 +0545

    notification admin

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = (
+        "recipient",
+        "verb",
+        "is_read",
+        "created_at",
+    )
+    list_filter = (
+        "is_read",
+        "created_at",
+    )
+    search_fields = (
+        "recipient__username",
+        "recipient__email",
+        "verb",
+        "description",
+    )
+    readonly_fields = ("created_at",)
+
+    fieldsets = (
+        (
+            "Notification Information",
+            {
+                "fields": (
+                    "recipient",
+                    "verb",
+                    "description",
+                    "target_url",
+                    "is_read",
+                )
+            },
+        ),
+        (
+            "Timestamps",
+            {
+                "fields": ("created_at",),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    ordering = ("-created_at",)
+    date_hierarchy = "created_at"
