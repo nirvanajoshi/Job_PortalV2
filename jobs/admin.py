eaf670e0bcd814b1c1744ac667e29d14245d8e38
nirvanajoshi <damodarj36@gmail.com>
2026-08-18 20:10:33 +0545

    jobs admin

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from .models import Job
 
-# Register your models here.
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "company",
+        "job_type",
+        "location",
+        "is_active",
+        "posted_at",
+    )
+    list_filter = (
+        "job_type",
+        "is_active",
+        "posted_at",
+    )
+    search_fields = (
+        "title",
+        "description",
+        "company__name",
+        "location",
+    )
+    readonly_fields = ("posted_at", "updated_at")
+    
+    fieldsets = (
+        ("Job Details", {
+            "fields": (
+                "company",
+                "title",
+                "description",
+                "job_type",
+                "location",
+                "salary_range",
+            )
+        }),
+        ("Status", {
+            "fields": (
+                "is_active",
+            )
+        }),
+        ("Timestamps", {
+            "fields": (
+                "posted_at",
+                "updated_at",
+            ),
+            "classes": ("collapse",),
+        }),
+    )
+    
+    ordering = ("-posted_at",)
+    date_hierarchy = "posted_at"
