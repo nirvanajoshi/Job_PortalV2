a2738a3e176070a8d6cd663e420ffa667a179ecf
nirvanajoshi <damodarj36@gmail.com>
2026-08-18 20:06:49 +0545

    profile admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,79 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import JobSeekerProfile
+
+
+@admin.register(JobSeekerProfile)
+class JobSeekerProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "headline",
+        "location",
+        "experience_years",
+        "created_at",
+    )
+
+    list_filter = (
+        "experience_years",
+        "created_at",
+    )
+
+    search_fields = (
+        "user__username",
+        "user__email",
+        "headline",
+        "location",
+        "skills",
+    )
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+
+    fieldsets = (
+        (
+            "Profile Information",
+            {
+                "fields": (
+                    "user",
+                    "headline",
+                    "bio",
+                    "location",
+                    "skills",
+                    "experience_years",
+                    "education",
+                )
+            },
+        ),
+        (
+            "Social Links",
+            {
+                "fields": (
+                    "website",
+                    "linkedin_url",
+                    "github_url",
+                )
+            },
+        ),
+        (
+            "Resume",
+            {
+                "fields": ("resume",),
+            },
+        ),
+        (
+            "Timestamps",
+            {
+                "fields": (
+                    "created_at",
+                    "updated_at",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    ordering = ("-created_at",)
+
+    date_hierarchy = "created_at"
