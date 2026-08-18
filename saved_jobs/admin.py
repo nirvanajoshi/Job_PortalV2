0426a5713cff0551cc2e0f6683ab938806e6cf8e
nirvanajoshi <damodarj36@gmail.com>
2026-08-18 20:13:27 +0545

    saved_jobs admin

--- saved_jobs/admin.py
+++ saved_jobs/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import SavedJob
 
-# Register your models here.
+
+@admin.register(SavedJob)
+class SavedJobAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "job",
+        "saved_at",
+    )
+    list_filter = ("saved_at",)
+    search_fields = (
+        "user__username",
+        "user__email",
+        "job__title",
+        "job__company__name",
+    )
+    readonly_fields = ("saved_at",)
+
+    fieldsets = (
+        (
+            "Saved Job Details",
+            {
+                "fields": (
+                    "user",
+                    "job",
+                )
+            },
+        ),
+        (
+            "Timestamps",
+            {
+                "fields": ("saved_at",),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    ordering = ("-saved_at",)
+    date_hierarchy = "saved_at"
