0b15d56ecc40a88a49de3ee9b54b452382d833c8
nirvanajoshi <damodarj36@gmail.com>
2026-08-18 20:05:56 +0545

    Application admin

--- applications/admin.py
+++ applications/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Application
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = (
+        "job",
+        "job_seeker",
+        "status",
+        "applied_at",
+    )
+
+    list_filter = (
+        "status",
+        "applied_at",
+    )
+
+    search_fields = (
+        "job__title",
+        "job_seeker__user__username",
+        "job_seeker__user__email",
+    )
+
+    readonly_fields = (
+        "applied_at",
+        "updated_at",
+    )
+
+    fieldsets = (
+        (
+            "Application Information",
+            {
+                "fields": (
+                    "job",
+                    "job_seeker",
+                    "resume",
+                    "cover_letter",
+                    "status",
+                )
+            },
+        ),
+        (
+            "Timestamps",
+            {
+                "fields": (
+                    "applied_at",
+                    "updated_at",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    ordering = ("-applied_at",)
+
+    date_hierarchy = "applied_at"
